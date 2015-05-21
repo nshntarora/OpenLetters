e4082a23e2fe1fac80594f9fe640f43dc3927b4f
Nishant Arora <neo.nishant7@gmail.com>
2015-05-21 15:43:18 +0530

    Resolved issues caused because of last commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414163717) do
+ActiveRecord::Schema.define(version: 20150521100344) do
 
   create_table "comments", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150414163717) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author"
   end
 
 end
